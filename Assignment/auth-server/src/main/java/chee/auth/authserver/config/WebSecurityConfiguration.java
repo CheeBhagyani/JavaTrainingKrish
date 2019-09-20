de896e520776765b58441507961df51ed3c0b220
pasindusandakelum <cheebhagyani1994@gmail.com>
2019-09-20 18:39:01 +0530

    UI changes

---
+++ Assignment/auth-server/src/main/java/chee/auth/authserver/config/WebSecurityConfiguration.java
@@ -0,0 +1,6 @@
+package chee.auth.authserver.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
