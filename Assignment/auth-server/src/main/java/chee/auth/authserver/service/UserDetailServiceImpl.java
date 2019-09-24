1c98b768850202b5631b4c38a50dc8ed33ae4ecd
pasindusandakelum <tmpsthilakarathna2017@gmail.com>
2019-09-24 11:44:07 +0530

    updated

---
+++ Assignment/auth-server/src/main/java/chee/auth/authserver/service/UserDetailServiceImpl.java
@@ -0,0 +1,23 @@
+package chee.auth.authserver.service;
+
+import chee.auth.authserver.model.User;
+import chee.auth.authserver.repository.UserDetailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserDetailRepository userDetailRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userDetailRepository.findByUsername(name);
+
+//        optionalUser.orElse(()-> new UsernameNotFoundException("Username or password is incorrect"));
+    }
+}
