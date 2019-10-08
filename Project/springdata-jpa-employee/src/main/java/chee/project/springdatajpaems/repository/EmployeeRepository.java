d6a7eea8f09c723e37c00bd0c7c29a0177702846
pasindusandakelum <cheebhagyani1994@gmail.com>
2019-10-08 16:30:41 +0530

    update

--- Project/springdata-jpa-employee/src/main/java/chee/project/springdatajpaems/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package chee.project.springdatajpaems.repository;
-import chee.rentcloud.ems.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
--- Project/springdata-jpa-project/src/main/java/chee/project/springdatajpaproject/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package chee.project.springdatajpaproject.repository;
-
-import chee.rentcloud.ems.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-}
