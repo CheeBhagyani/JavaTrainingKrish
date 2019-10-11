d8d8f0ae1ce80eba587bbc40b8fad4b4197f115a
pasindusandakelum <cheebhagyani1994@gmail.com>
2019-10-11 16:39:16 +0530

    Collections

---
+++ collection/src/SetTest.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetTest {
+    public static void main(String[] args) {
+        Set<Student> students = new HashSet<>();
+        students.add(new Student(8, "a" , "e"));
+        students.add(new Student(5, "a" , "e"));
+        students.add(new Student(6, "a" , "e"));
+        students.add(new Student(2, "b" , "f"));
+        students.add(new Student(4, "c" , "g"));
+        students.add(new Student(3, "d" , "h"));
+        students.add(new Student(1, "e" , "i"));
+
+        System.out.println("Before \n");
+        for(Student student: students ) {
+            Set<Student> studentList = new TreeSet<>();
+            studentList.add(student);
+        }
+        System.out.println(students);
+    }
+}
