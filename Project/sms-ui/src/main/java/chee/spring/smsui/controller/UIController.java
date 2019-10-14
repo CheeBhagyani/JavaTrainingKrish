72438ebc4e22e66ae8952ed97369d0a6b69b3951
pasindusandakelum <tmpsthilakarathna2017@gmail.com>
2019-10-14 15:02:53 +0530

    role based changes

--- Project/sms-ui/src/main/java/chee/spring/smsui/controller/UIController.java
+++ Project/sms-ui/src/main/java/chee/spring/smsui/controller/UIController.java
@@ -4,19 +4,14 @@ import chee.rentcloud.ems.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
@@ -41,7 +36,11 @@ public class UIController extends WebSecurityConfigurerAdapter {
                 .antMatchers("/")
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout").deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true);
+        http.sessionManagement().maximumSessions(1);
     }
 
     @PreAuthorize("hasRole('ROLE_admin') or hasRole('ROLE_operator')")
