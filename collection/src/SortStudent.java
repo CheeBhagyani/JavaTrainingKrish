6fb40bd01fd7c0afa06d30a3769d3ec8857181f1
Ruwanthie <cheebhagyani1994@gmail.com>
2019-10-18 21:44:32 +0530

    changes

--- collection/src/SortStudent.java
+++ collection/src/SortStudent.java
@@ -1,34 +1,4 @@
-import java.util.*;
+package PACKAGE_NAME;
 
 public class SortStudent {
-    public static void main(String[] args) {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student(8, "Chee" , "r"));
-        students.add(new Student(2, "Bhagyani" , "d"));
-        students.add(new Student(3, "Dasuni" , "a"));
-
-//        System.out.println("Before \n");
-//        for(Student student: students ) {
-//            List<Student> studentList = new ArrayList<>();
-//            studentList.add(student);
-//        }
-//        System.out.println(students);
-//
-//        System.out.println("\n After \n");
-//        List<Student> sortStudentList = new ArrayList<>();
-//        for(Student student: students ) {
-//            sortStudentList.add(student);
-//            student.compareTo(sortStudentList);
-//        }
-//        System.out.println(students);
-
-        Collections.sort(students);
-        System.out.println(students);
-    }
-//        Comparator<Student> studentComparator = (s1, s2) -> (s1.getId()< s2.getId()?-1:s1.getId()>s2.getId()?1:0);
-//        Collections.sort(students,studentComparator);
-//        System.out.println("After" +students);
-//    }
-
-
 }
